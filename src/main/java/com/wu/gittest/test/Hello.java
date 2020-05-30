7affd8f644f0b75cff22a9393a5a58ffc6bcd89f
WuMonkey <1610988176@qq.com>
2020-05-30 09:55:37 +0800

    创建分支

--- src/main/java/com/wu/gittest/test/Hello.java
+++ src/main/java/com/wu/gittest/test/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("555");
         System.out.println("666");
+        System.out.println("创建分支");
+
 
     }
 }
